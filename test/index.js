0afd307bc93204f8d2a482dbb5f29579229250e4
Michael Cavalea <callmecavs@gmail.com>
2018-08-05 20:38:42 -0400

    add simple test case

--- test/index.js
+++ test/index.js
@@ -1,9 +1,16 @@
 const expect = require('chai').expect
+const intersect = require('../dist/array-intersect.js')
 
 /* eslint-env mocha */
 
 describe('intersect', () => {
-  it('should have tests', () => {
-    expect(true).to.equal(true)
+  it('should find intersection of arrays', () => {
+    const one = [1, 2, 3]
+    const two = [2, 3, 4]
+
+    const test = intersect(one, two)
+    const expected = [2, 3]
+
+    expect(test).to.deep.equal(expected)
   })
 })
