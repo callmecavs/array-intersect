7fb8c29ad6d6488d5bc03a834174b04118855439
Michael Cavalea <callmecavs@gmail.com>
2018-08-05 20:38:04 -0400

    first take

--- src/array-intersect.js
+++ src/array-intersect.js
@@ -1,5 +1,7 @@
-const intersect = () => {
-
+const intersect = (start, ...rest) => {
+  return rest.reduce((accum, current) => {
+    return accum.filter(x => current.includes(x))
+  }, start)
 }
 
 export default intersect
