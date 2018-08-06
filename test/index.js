18381021e858a396cfedc28d78204261aa7bdfb8
Michael Cavalea <callmecavs@gmail.com>
2018-08-05 20:43:16 -0400

    another test case

--- test/index.js
+++ test/index.js
@@ -4,11 +4,22 @@ const intersect = require('../dist/array-intersect.js')
 /* eslint-env mocha */
 
 describe('intersect', () => {
-  it('should find intersection of arrays', () => {
+  it('should find intersection of 2 arrays', () => {
     const one = [1, 2, 3]
     const two = [2, 3, 4]
 
     const test = intersect(one, two)
+    const expected = [2, 3]
+
+    expect(test).to.deep.equal(expected)
+  })
+
+  it('should find intersection of more than 2 arrays', () => {
+    const one = [0, 1, 2, 3]
+    const two = [1, 2, 3, 4]
+    const three = [2, 3, 4, 5]
+
+    const test = intersect(one, two, three)
     const expected = [2, 3]
 
     expect(test).to.deep.equal(expected)
